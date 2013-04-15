d303137fe07449ca5dc012b1183a787860d69e33
Simon Gomizelj <simongmzlj@gmail.com>
2013-04-15 03:47:27 -0400

    cleanup test.cc

--- test.cc
+++ test.cc
@@ -3,21 +3,21 @@
 
 #include "ini.hh"
 
-static void print_arg(setting &s)
+static void print_arg(const setting &s)
 {
-  s.match([](const bool &v) { printf("bool: %s\n", v ? "true" : "false"); },
-          [](const long &v) { printf("unsigned: %ld\n", v); },
-          [](const std::string &v) { printf("string: %s\n", v.c_str()); });
+  s.match([](const bool v) { printf("%s [bool]\n", v ? "true" : "false"); },
+          [](const long v) { printf("%ld [long]\n", v); },
+          [](const std::string &v) { printf("%s [string]\n", v.c_str()); });
 }
 
 int main(void)
 {
   /* auto settings = read_conf("/home/simon/.config/termite/config"); */
-  auto settings = read_conf("test.ini");
+  const auto settings = read_conf("test.ini");
 
   printf(">> DUMPING\n");
   for (auto& x: settings) {
-      printf("key[%s] ", x.first.c_str());
+      printf("%s = ", x.first.c_str());
       print_arg(x.second);
   }
 
